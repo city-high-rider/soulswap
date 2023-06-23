2092dd21ef2eb4a08372eaec6d56b35bd8c87702
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-06-23 12:36:55 +1200

    create simple player body

--- components/possessable_body.gd
+++ components/possessable_body.gd
@@ -13,3 +13,15 @@ class_name PossessableBody
 # What's the main health component? If its health drops to zero, the body dies.
 @export var main_health : HealthComponent
 
+# What's the mouse sensitivity?
+var mouse_sens : float = 0.1
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+# We probably want everything to have the ability to look around.
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		body_camera.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		# Clamp the camera's rotation so that we can't look over 90 degrees up.
+		body_camera.rotation.x = clamp(body_camera.rotation.x, deg_to_rad(-89), deg_to_rad(80))
