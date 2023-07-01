fcb03e76eeef150e14eec00c6c7eecad2f23f724
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-02 10:17:07 +1200

    make text box fade after a while

--- components/UI/text_box.gd
+++ components/UI/text_box.gd
@@ -6,5 +6,17 @@ class_name TextBox
 # Reference to the label
 @onready var label : Label = $Label
 
+# How long is the text shown for?
+@onready var display_time : float = 6
+
+func _ready() -> void:
+	hide()
+
 func set_text(new_text: String) -> void:
+	show()
 	label.text = new_text
+	fade_after(display_time)
+	
+func fade_after(time_seconds: float) -> void:
+	await get_tree().create_timer(time_seconds).timeout
+	hide()
