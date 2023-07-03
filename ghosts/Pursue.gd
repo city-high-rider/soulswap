a946409b6ae1669c80efcb6b62773a3042f4ce14
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-03 13:49:34 +1200

    fix bug in ai movement

--- ghosts/Pursue.gd
+++ ghosts/Pursue.gd
@@ -7,13 +7,14 @@ extends State
 # What's our target? Hard coded for now
 @export var target : Shell
 
-func handle_physics(_delta: float) -> void:
+func handle_physics(delta: float) -> void:
 	var current_location : Vector3 = user.global_transform.origin
 	var next_location : Vector3 = nav_agent.get_next_path_position()
 	# Where we're going to go is our velocity times the basis. So we need to multiply by inverse basis when we are converting to inputs.
-	var next_velocity : Vector3 = (next_location - current_location) * user.global_transform.basis.inverse()
-	user.input_direction = Vector2(-next_velocity.x, next_velocity.z).normalized()
-
+	var next_velocity : Vector3 = (next_location - current_location) * user.transform.basis.inverse()
+	user.input_direction = Vector2(next_velocity.x, -next_velocity.z).normalized()
+	
+	
 
 func _on_target_update_timer_timeout():
 	nav_agent.set_target_position(target.global_transform.origin)
