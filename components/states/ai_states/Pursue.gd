cc876cb2ac8910d6aa6b6fdc417fcb5faa0bd917
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-04 18:41:10 +1200

    create more general ai state class

--- components/states/ai_states/Pursue.gd
+++ components/states/ai_states/Pursue.gd
@@ -1,4 +1,4 @@
-extends State
+extends AiState
 # This is a state that just makes the enemy beeline towards the player.
 
 # Link to the navigation agent that we are using.
@@ -22,19 +22,9 @@ func handle_physics(delta: float) -> void:
 		var next_location : Vector3 = nav_agent.get_next_path_position()
 		user.input_direction = input_move_towards(next_location)
 		# Let's look at the player while we're at it.
-		look_towards(target.global_transform.origin, delta)
+		user.mouse_direction = look_towards_y(target.global_transform.origin, rad_to_deg(TAU), delta)
 	else:
 		user.input_direction = Vector2.ZERO
 		user.mouse_direction = Vector2.ZERO
 		target = PlayerInfo.current_player_shell
-	
-	
-func look_towards(pos: Vector3, delta: float) -> void:
-	var local_pos : Vector3 = user.to_local(pos)
-	var y_angle : float = Vector3.FORWARD.angle_to(local_pos * Vector3(1, 0, 1)) * sign(local_pos.x)
-	user.mouse_direction = Vector2(sign(y_angle) * min(delta * rad_to_deg(TAU), abs(rad_to_deg(y_angle))),0)
-	
-func input_move_towards(destination: Vector3) -> Vector2:
-	# that function gives us a path position in global space, but we need it in local space.
-	var next_velocity : Vector3 = user.to_local(destination)
-	return Vector2(next_velocity.x, -next_velocity.z).normalized()
+
