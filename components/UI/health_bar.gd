8f4d1a5a901dbccdd277038656d10d4ec8ac579e
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-06 11:11:45 +1200

    Make healthbar text editable

--- components/UI/health_bar.gd
+++ components/UI/health_bar.gd
@@ -1,3 +1,4 @@
+@tool
 extends HBoxContainer
 class_name HealthBar
 
@@ -5,17 +6,24 @@ class_name HealthBar
 # Link to the actual bar child.
 @onready var bar : ProgressBar = $ProgressBar
 
+@onready var label : Label = $Label
+
 # Which health component are we tracking?
 @export var health : HealthComponent
+
+# What should the text next to the bar say?
+@export var text : String = "Health"
 
 func _ready() -> void:
 	calibrate()
 	health.health_changed.connect(_on_health_changed)
+	label.text = text
 
 # Set the bar's max value and value to match the component.
 func calibrate() -> void:
 	bar.max_value = health.max_health
 	bar.value = health.current_health
+	
 	
 func _on_health_changed(new_health) -> void:
 	bar.value = new_health
