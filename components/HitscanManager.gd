245439c1f6513a8c9e5fef0189d68acd8bfb2ac9
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-05 21:02:29 +1200

    added a hitscan manager

---
+++ components/HitscanManager.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name HitscanManager
+
+# this is a node that manages hitscan weapons. It is responsible for calling the
+# take_damage function on hitboxes and drawing tracers.
+
+# which raycast should we use to check for collisions?
+@export var raycast : RayCast3D
+
+# this function should be called by a hitscan weapon when it fires, to which
+# it will pass its damage. We will check for a hit, and if there is one,
+# we pass some damage. Returns whether or not we hit an enemy.
+func check_hit(damage: float) -> bool:
+	var collider = raycast.get_collider()
+	if !collider:
+		return false
+	
+	if collider is Hitbox:
+		collider.take_damage(damage)
+	return false
