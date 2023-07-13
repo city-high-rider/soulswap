6ec7701747a5822c8f76c8e0b3eb408f475d3fc3
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-13 17:35:01 +1200

    make body state machine responsible for calling down state

--- components/states/body_state_machine.gd
+++ components/states/body_state_machine.gd
@@ -5,6 +5,9 @@ class_name BodyStateMachine
 ## Which mount is the ghost connected to?
 @export var ghost_mount : GhostMount
 
+## What is the default death state?
+@export var down_state : ShellState
+
 func _ready() -> void:
 	ghost_mount.ghost_changed.connect(_on_ghost_changed)
 	ghost_mount.ghost_emitted_output.connect(handle_ghost_input)
@@ -22,3 +25,6 @@ func _physics_process(delta: float) -> void:
 
 func handle_ghost_input(action: String, payload) -> void:
 	current_state.handle_ghost_output(action, payload)
+
+func _on_health_component_died() -> void:
+	switch_state(down_state)
--- components/states/shell_state.gd
+++ components/states/shell_state.gd
@@ -2,18 +2,8 @@ extends State
 ## State that controls how the shell moves. Has access to a ghost.
 class_name ShellState
 
-# We are using an export, but it might make more sense (and be more convenient)
-# later down the line to have the BodyStateMachine expose this and set it at runtime.
-## which state do we transition to when our health drops to zero?
-@export var down_state : ShellState
-
 # Which ghost are we reading inputs from? Set at runtime.
 var ghost : Ghost
 
 func handle_ghost_input(action: String, payload) -> void:
 	pass
-
-# this is the most convenient place to check for death, because we don't need to 
-# copy this code over a bunch of different states.
-func _on_health_component_died() -> void:
-	state_machine.change_state(down_state)
