0fb2cddc1caa817b7430feb9b05729aafea4feda
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-14 18:22:58 +1200

    remove unused signal

--- components/GhostMount.gd
+++ components/GhostMount.gd
@@ -1,24 +1,21 @@
 extends Node3D
 class_name GhostMount
 
-# While the body's movement logic and the ghost should be separate, there is still some information
-# that they need to share. The ghost should be a child of this node, and other nodes can connect to it
-# such as the state machine. By connecting everything to one spot, it makes it easy to change out the ghost.
+## While the body's movement logic and the ghost should be separate, there is still some information
+## that they need to share. The ghost should be a child of this node, and other nodes can connect to it
+## such as the state machine. By connecting everything to one spot, it makes it easy to change out the ghost.
 
-# What is our ghost?
+## What is our ghost?
 @export var ghost : Ghost 
 
-# Which body are we connecting to the ghost?
+## Which body are we connecting to the ghost?
 @export var shell : Shell
 
-# Emit this signal when the ghost changes, so that we can re-initialise state machines, etc.
+## Emit this signal when the ghost changes, so that we can re-initialise state machines, etc.
 signal ghost_changed(new_ghost: Ghost, is_player: bool)
 
-# Emit this signal when the ghost outputs anything
+## Emit this signal when the ghost outputs anything
 signal ghost_emitted_output(action: String, payload)
-
-# Emit this signal when the shell's main health drops to zero (dies)
-signal shell_died
 
 func _ready() -> void:
 	change_ghost(ghost)
