3f4ed85f08807155ff9ad210ce7c1082de4c1183
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-14 13:12:01 +1200

    make spawner spawn stuff

--- interactive/enemy_spawner.gd
+++ interactive/enemy_spawner.gd
@@ -10,14 +10,23 @@ var is_used : bool = false
 
 var saved_state : bool = false
 
+# Reference to the spawned enemy.
+var spawned_entity : Node
+
 func _ready() -> void:
 	CheckpointManager.checkpoint_activated.connect(save_state)
 	CheckpointManager.load_checkpoint.connect(load_state)
 
+func spawn() -> void:
+	if spawned_entity:
+		spawned_entity.queue_free()
+	spawned_entity = enemy.instantiate()
+	add_child(spawned_entity)
 
 func save_state() -> void:
 	saved_state = is_used
-	
 
 func load_state() -> void:
 	is_used = saved_state
+	if !is_used:
+		spawned_entity.queue_free()
