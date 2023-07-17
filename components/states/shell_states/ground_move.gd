dbc930da8a937b2e8c43a84172a072c669ca7942
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-18 11:00:19 +1200

    tweak gravity and jump height

--- components/states/shell_states/ground_move.gd
+++ components/states/shell_states/ground_move.gd
@@ -2,8 +2,11 @@ extends MoveState
 
 # State specifically for grounded movement, that can jump and fall etc.
 
-# Which state to transition to when we fall?
+## Which state to transition to when we fall?
 @export var fall_state : AirState
+
+## How high do we jump?
+@export_range(0,10,0.1) var jump_height : float = 2
 
 func handle_physics(delta: float) -> void:
 	super(delta)
@@ -13,5 +16,5 @@ func handle_physics(delta: float) -> void:
 func handle_ghost_output(action: String, _payload) -> void:
 	match action:
 		"jump":
-			user.velocity.y += 10
+			user.velocity.y += jump_height
 			state_machine.switch_state(fall_state)
