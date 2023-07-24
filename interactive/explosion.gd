f0456372241d4c66e984357d20c3f85c535a397e
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-24 13:57:13 +1200

    make explosion

---
+++ interactive/explosion.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("explode")
+	await animation_player.animation_finished
+	queue_free()
