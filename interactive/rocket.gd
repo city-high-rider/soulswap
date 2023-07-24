e27b08b4475a18857cd27e12296ec6dfc25af5de
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-24 13:43:25 +1200

    made rocket delete itself on contact

--- interactive/rocket.gd
+++ interactive/rocket.gd
@@ -1,5 +1,16 @@
 extends CharacterBody3D
 
+@export var speed : float = 10
+@export var explosion : PackedScene
+
+@onready var raycast : RayCast3D = $RayCast3D
 func _physics_process(delta: float) -> void:
-	velocity = -global_transform.basis.z * 5
+	velocity = -global_transform.basis.z * speed
 	move_and_slide()
+	
+	if raycast.is_colliding():
+		if explosion:
+			var new_explosion = explosion.instantiate()
+			get_node("/root").add_child(new_explosion)
+			new_explosion.global_transform.origin = global_transform.origin
+		queue_free()
