5ff3bd2f21daa236558e6b831e058d3d49d7c842
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-25 14:44:30 +1200

    add blast to explosion

--- interactive/explosion.gd
+++ interactive/explosion.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var blast_radius : Area3D = $BlastRadius
 
 func _ready() -> void:
 	animation_player.play("explode")
 	await animation_player.animation_finished
 	queue_free()
+
+
+func _on_blast_radius_area_entered(area):
+	if area is Hitbox:
+		area.take_damage(10, null)
