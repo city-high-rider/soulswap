28f260bd3119b64ae2d0a6fc139e377902a6c0f5
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-27 12:01:35 +1200

    add sliding door

---
+++ interactive/sliding_door.gd
@@ -0,0 +1,29 @@
+extends Node3D
+## This is a simple animated sliding door.
+
+
+## Reference to the animation player
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+## Is this door initially open or closed?
+@export var initially_open : bool = false
+var is_open : bool = initially_open
+
+func _ready() -> void:
+	if initially_open:
+		open()
+	else:
+		close()
+		
+func open() -> void:
+	if is_open:
+		return
+	is_open = true
+	animation_player.play("open")
+	
+func close() -> void:
+	if !is_open:
+		return
+	is_open = false
+	animation_player.play("close")
+	
