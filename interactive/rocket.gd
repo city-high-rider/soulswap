47e3c8b548118f6eef83d6144a777503a1d35e0e
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-07-29 14:30:39 +1200

    change rocket fire sound and add rocket booster sound

--- interactive/rocket.gd
+++ interactive/rocket.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 
-@export var speed : float = 10
+@export var speed : float = 20
 @export var explosion : PackedScene
 
 var despawn_time : float = 10
 
 @onready var detonate_area : Area3D = $Area3D
+@onready var booster_sound : AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _ready() -> void:
 	get_tree().create_timer(despawn_time).timeout.connect(queue_free)
@@ -14,7 +15,6 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	velocity = -global_transform.basis.z * speed
 	move_and_slide()
-	
 
 
 func _on_area_3d_body_entered(body):
@@ -23,3 +23,7 @@ func _on_area_3d_body_entered(body):
 		get_node("/root").add_child(new_explosion)
 		new_explosion.global_transform.origin = global_transform.origin
 	queue_free()
+
+# Make sure the sound loops
+func _on_audio_stream_player_3d_finished():
+	booster_sound.play()
