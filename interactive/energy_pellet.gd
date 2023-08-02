cae96678e1089b19e9027217cdedf01ba9659b02
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-02 19:42:02 +1200

    despawn energy pellet when it hits body

--- interactive/energy_pellet.gd
+++ interactive/energy_pellet.gd
@@ -5,3 +5,9 @@ extends Node3D
 func _on_area_3d_area_entered(area):
 	if area is Hitbox:
 		area.take_damage(10, null)
+		queue_free()
+
+
+func _on_area_3d_body_entered(body):
+#	queue_free()
+	pass
