b7324edb9944e6f71ad49c05e96a12c0718a345a
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-21 10:44:07 +1200

    make explosions deal explosive damage

--- interactive/explosion.gd
+++ interactive/explosion.gd
@@ -4,8 +4,13 @@ extends Node3D
 @onready var blast_radius : Area3D = $BlastRadius
 
 @onready var explosion_sound : AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 ## How far should we launch stuff that gets caught in the explosion?
 @export var knockback : float = 4
+
+## How much damage does the explosion do?
+@export var damage : Damage = Damage.new()
+
 func _ready() -> void:
 	animation_player.play("explode")
 	explosion_sound.play()
