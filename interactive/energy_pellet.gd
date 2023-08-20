8eefbe78b407ec02086e467e9604aeb9f6d9d967
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-21 10:51:13 +1200

    make energy pellet use new damage system

--- interactive/energy_pellet.gd
+++ interactive/energy_pellet.gd
@@ -1,10 +1,12 @@
 extends Node3D
 ## this is for the energy pellet projectile.
 
+## How much damage do we deal?
+@export var damage : Damage = Damage.new()
 
 func _on_area_3d_area_entered(area):
 	if area is Hitbox:
-		area.take_damage(10, null)
+		area.take_damage(damage, null)
 		queue_free()
 
 
