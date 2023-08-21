e51782f5d5ddff7ecd6671580befad7460fc1458
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-21 14:09:09 +1200

    make hitbox responsible for registering what hit the player

--- components/hitbox.gd
+++ components/hitbox.gd
@@ -6,9 +6,13 @@ class_name Hitbox
 ## Which health component are we connecting to?
 @export var health_component : HealthComponent
 
-## By how much should we multiply all incoming damage? useful for weak points, like a 
+## By how much should we multiply *all* incoming damage? useful for weak points, like a 
 ## headshot hitbox.
 @export var damage_multiplier : float = 1
+
+## We are responsible for relaying information about the attack's origin.
+## Signal when we take damage from a distinct source
+signal took_damage(lethal: bool, damage: Damage, attacker)
 
 # Note that right now we have separate exports for each damage type.. if this grows we should change
 # to something like an array of tuples. I didn't do that right now because godot doesn't have built in tuples.
@@ -23,8 +27,10 @@ class_name Hitbox
 @export var plasma_mult : float = 1
 
 
-# function that is called when we take damgae.
-func take_damage(damage : Damage, source) -> void:
+# function that is called when we take damgae
+# damage is the damage object, attacker is usually the shell that attacked us
+func take_damage(damage : Damage, attacker) -> void:
+	# Calculate the damage multiplier given our damage resistances defaulting to 1.
 	var specific_type_multiplier : float = 1
 	match damage.damage_type:
 		damage.DamageType.Kinetic:
@@ -35,4 +41,8 @@ func take_damage(damage : Damage, source) -> void:
 			specific_type_multiplier = plasma_mult
 			
 	if health_component:
-		health_component.take_damage(damage.damage_amount * damage_multiplier * specific_type_multiplier, source)
+		var was_damage_lethal : bool = health_component.take_damage(damage.damage_amount * damage_multiplier * specific_type_multiplier)
+		took_damage.emit(was_damage_lethal, damage, attacker)
+		
+		
+		
