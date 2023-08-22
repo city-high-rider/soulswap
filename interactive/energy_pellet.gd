8860a1fa7481d46e26fed49cd3682703e8e1f6dc
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-23 10:36:42 +1200

    change energy pellet to characterbody

--- interactive/energy_pellet.gd
+++ interactive/energy_pellet.gd
@@ -1,15 +1,6 @@
-extends Node3D
+extends Projectile
 ## this is for the energy pellet projectile.
 
-## How much damage do we deal?
-@export var damage : Damage = Damage.new()
-
-func _on_area_3d_area_entered(area):
-	if area is Hitbox:
-		area.take_damage(damage, null)
-		queue_free()
-
-
-func _on_area_3d_body_entered(body):
-#	queue_free()
-	pass
+func _init() -> void:
+	on_collide = queue_free
+	on_hitbox_collide = queue_free
