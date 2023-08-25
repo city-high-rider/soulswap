68ba588f9db94cdcb6a073e8b09b71d7b5d0d018
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-25 18:45:11 +1200

    make checkpoint have light

--- interactive/checkpoint.gd
+++ interactive/checkpoint.gd
@@ -10,6 +10,9 @@ class_name Checkpoint
 # Reference to the particles
 @onready var particles : GPUParticles3D = $Lantern/CSGBox3D3/GPUParticles3D
 
+# Reference to the light animation 
+@onready var light_flicker : AnimationPlayer = $Lantern/CSGBox3D3/AnimationPlayer
+
 # Emit this signal when a player enters the checkpoint.
 signal checkpoint_activated(player: Shell)
 
@@ -19,6 +22,7 @@ func _on_area_3d_body_entered(body) -> void:
 		checkpoint_activated.emit(body)
 		CheckpointManager.checkpoint_activated.emit()
 		particles.set_emitting(true)
+		light_flicker.play("light_flicker")
 		
 		if ambient_audio.finished.is_connected(ambient_audio.play):
 			return
