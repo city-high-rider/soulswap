65f356b425b639265f8fb6d7d74696cab0c0321b
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-26 19:37:34 +1200

    apply buff correctly

--- components/buffs/DroneBuff.gd
+++ components/buffs/DroneBuff.gd
@@ -1,11 +1,13 @@
 extends ShellBuff
 
 ## How much should we multiply max health by?
-@export var extra_hp_mult : float = 2
+@export var extra_hp_mult : float = 10
 
 func on_apply_change_health(health_component : HealthComponent) -> void:
 	health_component.max_health *= extra_hp_mult
+	health_component.current_health = health_component.max_health
 
 	
 func on_wearoff_change_health(health_component : HealthComponent) -> void:
 	health_component.max_health /= extra_hp_mult
+	health_component.current_health = health_component.max_health
