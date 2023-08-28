8a84c2a7f2d1d406c16f5eacc85bd928b88c4be8
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2023-08-28 18:18:25 +1200

    initialise health differently

--- components/modifiable_health_component.gd
+++ components/modifiable_health_component.gd
@@ -7,8 +7,9 @@ extends HealthComponent
 var original_max_hp : float = max_health
 
 func _ready() -> void:
-	recalibrate_max_health(ghost_mount.get_ghost_modifiers().max_health_multiplier)
 	super()
+	original_max_hp = max_health
+	recalibrate_max_health(ghost_mount.get_ghost_modifiers().max_health_multiplier)
 	ghost_mount.ghost_changed.connect(func (_new_ghost, _is_player): recalibrate_max_health(ghost_mount.get_ghost_modifiers().max_health_multiplier))
 	ghost_mount.ghost_cleared.connect(func(): recalibrate_max_health(1))
 	
